460ada49cc6f299bd1d18df87116a416ca6e9bb6
Maxime Wack <maximewack@free.Fr>
2017-02-08 12:05:31 -0500

    Duplicated line

--- R/metadata.R
+++ R/metadata.R
@@ -6,7 +6,6 @@ clear_metadata <- function(host = "localhost", admin, pass)
   RPostgreSQL::dbGetQuery(metadata, "DROP TABLE custom_meta;")
   RPostgreSQL::dbGetQuery(metadata, "DROP TABLE i2b2;")
   RPostgreSQL::dbGetQuery(metadata, "DROP TABLE icd10_icd9;")
-  RPostgreSQL::dbGetQuery(metadata, "DROP TABLE icd10_icd9;")
 
   RPostgreSQL::dbGetQuery(metadata, "DELETE FROM schemes;")
   RPostgreSQL::dbGetQuery(metadata, "DELETE FROM table_access;")
