1a9b3f4ea3066551839b7fc041410db6201bbf93
Maxime Wack <maximewack@free.Fr>
2017-02-10 00:16:06 +0100

    Moved and renamed delete_ontology

--- R/ontology.R
+++ R/ontology.R
@@ -22,3 +22,27 @@ add_ontology <- function(host = "127.0.0.1", admin, pass, name, scheme, descript
 
   populate_concept(host, admin, pass, ont, modi, name, scheme)
 }
+
+#' Delete an ontology from metadata
+#'
+#' Delete an existing ontology from metadata
+#'
+#' Delete the corresponding table
+#' Delete the scheme in schemes table
+#' Delete the entry in table_acess
+#'
+#' @param host The host to connect to
+#' @param admin The admin account for the PostgreSQL database
+#' @param pass the password for the admin account
+#' @param scheme The scheme to use for this ontology
+#' @export
+delete_ontology <- function(host = "127.0.0.1", admin, pass, scheme)
+{
+  metadata   <- RPostgreSQL::dbConnect(RPostgreSQL::PostgreSQL(), host = host, dbname = "i2b2metadata", user = admin, password = pass)
+
+  RPostgreSQL::dbGetQuery(metadata, stringr::str_c("DROP TABLE ", scheme, ";"))
+  RPostgreSQL::dbGetQuery(metadata, stringr::str_c("DELETE FROM table_access WHERE (c_table_cd = '", scheme, "');"))
+  RPostgreSQL::dbGetQuery(metadata, stringr::str_c("DELETE FROM schemes WHERE (c_name = '", scheme, "');"))
+
+  RPostgreSQL::dbDisconnect(metadata)
+}
