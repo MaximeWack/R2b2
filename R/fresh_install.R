218192979212c5ce2e60b57769d415dcec04eb91
Maxime Wack <maximewack@free.Fr>
2017-02-09 22:29:11 +0100

    Added a fresh_install() function and add_ontology() function

---
+++ R/fresh_install.R
@@ -0,0 +1,40 @@
+#' Make a fresh install from the base VM
+#'
+#' Make a fresh install from the base VM
+#'
+#' Set the permissions,
+#' create the admin account for the system, database, and i2b2
+#' Set the domain and project
+#' Delete the default users
+#' Clear the metadata database, and the *_dimension tables in demodata
+#'
+#' @param host The host to connect to
+#' @param admin The admin account for the PostgreSQL database
+#' @param pass the password for the admin account
+#' @param domain_id The desired domain_id
+#' @param domain_name The desired domain_name
+#' @param project_id The desired project id
+#' @param project_name The desired project name
+#' @export
+fresh_install <- function(admin, pass, domain_id, domain_name, project_id, project_name)
+{
+  host <- "localhost"
+
+  set_permissions()
+
+  create_admin(admin, pass)
+
+  set_domain(admin, pass, domain_id, domain_name)
+
+  set_project(host, admin, pass, project_id, project_name)
+
+  add_users(domain_id, "i2b2", "demouser", data.frame(id = admin, password = pass, name = admin, email = "", role = "ADMIN"))
+
+  delete_users(host, admin, pass, c("i2b2", "demo"))
+
+  clear_default_metadata(host, admin, pass)
+
+  clear_concept(host, admin, pass)
+
+  clear_modifier(host, admin, pass)
+}
---
+++ R/ontology.R
@@ -0,0 +1,24 @@
+#' Add an ontology to i2b2
+#'
+#' Add an ontology into i2b2
+#' Add the ontology table and entries in metadata
+#' Populate the ontology table
+#' Populate the concept and modifier dimensions in demodata
+#'
+#' @param host The host to connect to
+#' @param admin The admin account for the PostgreSQL database
+#' @param pass the password for the admin account
+#' @param name The name of the new ontology
+#' @param scheme The scheme to use for this ontology
+#' @param description The description of the scheme
+#' @param ont The ontology to insert
+#' @param modi The modifiers to insert
+#' @export
+add_ontology <- function(host = "localhost", admin, pass, name, scheme, description, ont, modi)
+{
+  add_ont(host, admin, pass, name, scheme, description)
+
+  populate_ont(host, admin, pass, ont, modi, name, scheme)
+
+  populate_concept(host, admin, pass, ont, modi, name, scheme)
+}
