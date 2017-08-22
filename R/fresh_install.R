f745c107bf97c2dbb36a2e5e5b61fc44f6bdc96b
Maxime Wack <maximewack@free.Fr>
2017-08-22 13:17:08 -0400

    Added a bunch of projects and users and data

--- R/fresh_install.R
+++ R/fresh_install.R
@@ -40,9 +40,9 @@ fresh_install <- function(admin, pass, domain_id, domain_name)
   # Prepare to clone i2b2demodata
   service("pg", "restart")
 
-  add_project("CHRU")
+  add_project("CHRU", "Tous services")
 
-  add_user_roles("i2b2", "demouser", "maxx", "CHRU", c("MANAGER", "USER", "DATA_PROT"))  
+  add_user_roles("i2b2", "demouser", "maxx", "CHRU", c("MANAGER", "USER", "DATA_PROT"))
 
   # Clean old users
   delete_users(c("i2b2","demo"))
@@ -75,43 +75,252 @@ fresh_install <- function(admin, pass, domain_id, domain_name)
   service("jboss", "restart")
 }
 
+accounts_obgyn <- function()
+{
+  add_user("i2b2", "demouser", "obgyn", "Gynécologie-Obstétrique", "", "obgyn")
+  add_user("i2b2", "demouser", "amp", "AMP Clinique", "", "amp")
+  add_user("i2b2", "demouser", "cancero", "Gynécologie et Cancérologie", "", "cancero")
+  add_user("i2b2", "demouser", "ortho", "Orthogénie", "", "ortho")
+  add_user("i2b2", "demouser", "ante", "Anténatal", "", "ante")
+  add_user("i2b2", "demouser", "endoc", "Endocrinologie Maternité", "", "endoc")
+  add_user("i2b2", "demouser", "post", "Post-natal", "", "post")
+  add_user("i2b2", "demouser", "nn", "Nouveaux-nés", "", "nn")
+
+  add_user_roles("i2b2", "demouser", "maxx", "620", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "maxx", "6040", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "maxx", "6050", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "maxx", "6060", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "maxx", "6070", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "maxx", "6080", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "maxx", "6090", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "maxx", "6100", c("MANAGER", "USER", "DATA_PROT"))
+
+  add_user_roles("i2b2", "demouser", "obgyn", "CHRU", c("USER", "DATA_AGG"))
+  add_user_roles("i2b2", "demouser", "obgyn", "620", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "obgyn", "6040", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "obgyn", "6050", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "obgyn", "6060", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "obgyn", "6070", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "obgyn", "6080", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "obgyn", "6090", c("MANAGER", "USER", "DATA_PROT"))
+  add_user_roles("i2b2", "demouser", "obgyn", "6100", c("MANAGER", "USER", "DATA_PROT"))
+
+  add_user_roles("i2b2", "demouser", "amp", "CHRU", c("USER", "DATA_OBFSC"))
+  add_user_roles("i2b2", "demouser", "amp", "620", c("USER", "DATA_AGG"))
+  add_user_roles("i2b2", "demouser", "amp", "6040", c("MANAGER", "USER", "DATA_PROT"))
+
+  add_user_roles("i2b2", "demouser", "cancero", "CHRU", c("USER", "DATA_OBFSC"))
+  add_user_roles("i2b2", "demouser", "cancero", "620", c("USER", "DATA_AGG"))
+  add_user_roles("i2b2", "demouser", "cancero", "6050", c("MANAGER", "USER", "DATA_PROT"))
+
+  add_user_roles("i2b2", "demouser", "ortho", "CHRU", c("USER", "DATA_OBFSC"))
+  add_user_roles("i2b2", "demouser", "ortho", "620", c("USER", "DATA_AGG"))
+  add_user_roles("i2b2", "demouser", "ortho", "6060", c("MANAGER", "USER", "DATA_PROT"))
+
+  add_user_roles("i2b2", "demouser", "ante", "CHRU", c("USER", "DATA_OBFSC"))
+  add_user_roles("i2b2", "demouser", "ante", "620", c("USER", "DATA_AGG"))
+  add_user_roles("i2b2", "demouser", "ante", "6070", c("MANAGER", "USER", "DATA_PROT"))
+
+  add_user_roles("i2b2", "demouser", "endoc", "CHRU", c("USER", "DATA_OBFSC"))
+  add_user_roles("i2b2", "demouser", "endoc", "620", c("USER", "DATA_AGG"))
+  add_user_roles("i2b2", "demouser", "endoc", "6080", c("MANAGER", "USER", "DATA_PROT"))
+
+  add_user_roles("i2b2", "demouser", "post", "CHRU", c("USER", "DATA_OBFSC"))
+  add_user_roles("i2b2", "demouser", "post", "620", c("USER", "DATA_AGG"))
+  add_user_roles("i2b2", "demouser", "post", "6090", c("MANAGER", "USER", "DATA_PROT"))
+
+  add_user_roles("i2b2", "demouser", "nn", "CHRU", c("USER", "DATA_OBFSC"))
+  add_user_roles("i2b2", "demouser", "nn", "620", c("USER", "DATA_AGG"))
+  add_user_roles("i2b2", "demouser", "nn", "6100", c("MANAGER", "USER", "DATA_PROT"))
+}
+
+
+pop_obgyn <- function()
+{
+  UM <- seq(6040, 6100, 10)
+
+  c(UM, 620) %>%
+    map(function(project)
+        {
+          populate_concept(readr::read_csv("../inst/cim.ont"), readr::read_csv("../inst/cim.modi"), "CIM", project)
+          populate_concept(readr::read_csv("../inst/ccam.ont"), modi = NULL, "CCAM", project)
+          populate_concept(readr::read_csv("../inst/bio.ont"), modi = NULL, "BIO", project)
+          populate_provider(readr::read_csv("../inst/struct.ont"), "STRUCT", project)
+          populate_concept(readr::read_csv("../inst/hospit.ont"), modi = NULL, "HOS", project)
+          populate_concept(readr::read_csv("../inst/patients.ont"), modi = NULL, "PAT", project)
+        }
+  )
+
+# 2016
+  readr::read_csv("/manip/pims16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+    stats::setNames(c("patient_ide", "encounter_ide", "start_date", "end_date", "sex_cd", "birth_date", "death_date", "rum_start", "rum_end", "provider_id", "project")) %>%
+    dplyr::filter(project %in% UM) ->
+  patients
+
+  patients %>%
+    import_patients_visits(620)
+
+  readr::read_csv("/manip/diags16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+    stats::setNames(c("patient_ide", "encounter_ide", "start_date", "end_date", "provider_id", "concept_cd", "modifier_cd")) %>%
+    semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+    import_diagnostics(620)
+
+  readr::read_csv("/manip/actes16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+    stats::setNames(c("patient_ide", "encounter_ide", "provider_id", "concept_cd", "start_date")) %>%
+    semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+  import_actes(620)
+
+  readr::read_csv("/manip/mensurations16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+    stats::setNames(c("patient_ide", "encounter_ide", "poids", "taille", "IMC")) %>%
+    semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+  import_mensurations(patients, 620)
+
+  readr::read_csv("/manip/bio16_1.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+    stats::setNames(c("patient_ide", "encounter_ide", "start_date", "concept_cd", "nval_num")) %>%
+    semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+  import_bios(patients, 620)
+
+  readr::read_csv("/manip/bio16_2.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+    stats::setNames(c("patient_ide", "encounter_ide", "start_date", "concept_cd", "nval_num")) %>%
+    semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+  import_bios(patients, 620)
+
+# 2017
+  readr::read_csv("/manip/pims17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+    stats::setNames(c("patient_ide", "encounter_ide", "start_date", "end_date", "sex_cd", "birth_date", "death_date", "rum_start", "rum_end", "provider_id", "project")) %>%
+    dplyr::filter(project %in% UM) ->
+  patients
+
+  patients %>%
+    import_patients_visits(620)
+
+  readr::read_csv("/manip/diags17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+    stats::setNames(c("patient_ide", "encounter_ide", "start_date", "end_date", "provider_id", "concept_cd", "modifier_cd")) %>%
+    semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+    import_diagnostics(620)
+
+  readr::read_csv("/manip/actes17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+    stats::setNames(c("patient_ide", "encounter_ide", "provider_id", "concept_cd", "start_date")) %>%
+    semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+  import_actes(620)
+
+  readr::read_csv("/manip/mensurations17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+    stats::setNames(c("patient_ide", "encounter_ide", "poids", "taille", "IMC")) %>%
+    semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+  import_mensurations(patients, 620)
+
+  readr::read_csv("/manip/bios17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+    stats::setNames(c("patient_ide", "encounter_ide", "start_date", "concept_cd", "nval_num")) %>%
+    semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+  import_bios(patients, 620)
+
+  UM %>%
+    map(function(x)
+        {
+          # 2016
+          readr::read_csv("/manip/pims16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+            stats::setNames(c("patient_ide", "encounter_ide", "start_date", "end_date", "sex_cd", "birth_date", "death_date", "rum_start", "rum_end", "provider_id", "project")) %>%
+            dplyr::filter(project == x) ->
+              patients
+
+            patients %>%
+              import_patients_visits(x)
+
+            readr::read_csv("/manip/diags16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+              stats::setNames(c("patient_ide", "encounter_ide", "start_date", "end_date", "provider_id", "concept_cd", "modifier_cd")) %>%
+              semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+              import_diagnostics(x)
+
+            readr::read_csv("/manip/actes16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+              stats::setNames(c("patient_ide", "encounter_ide", "provider_id", "concept_cd", "start_date")) %>%
+              semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+              import_actes(x)
+
+            readr::read_csv("/manip/mensurations16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+              stats::setNames(c("patient_ide", "encounter_ide", "poids", "taille", "IMC")) %>%
+              semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+              import_mensurations(patients, x)
+
+            readr::read_csv("/manip/bio16_1.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+              stats::setNames(c("patient_ide", "encounter_ide", "start_date", "concept_cd", "nval_num")) %>%
+              semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+              import_bios(patients, x)
+
+            readr::read_csv("/manip/bio16_2.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+              stats::setNames(c("patient_ide", "encounter_ide", "start_date", "concept_cd", "nval_num")) %>%
+              semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+              import_bios(patients, x)
+
+            # 2017
+            readr::read_csv("/manip/pims17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+              stats::setNames(c("patient_ide", "encounter_ide", "start_date", "end_date", "sex_cd", "birth_date", "death_date", "rum_start", "rum_end", "provider_id", "project")) %>%
+              dplyr::filter(project == x) ->
+                patients
+
+              patients %>%
+                import_patients_visits(x)
+
+              readr::read_csv("/manip/diags17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+                stats::setNames(c("patient_ide", "encounter_ide", "start_date", "end_date", "provider_id", "concept_cd", "modifier_cd")) %>%
+                semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+                import_diagnostics(x)
+
+              readr::read_csv("/manip/actes17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+                stats::setNames(c("patient_ide", "encounter_ide", "provider_id", "concept_cd", "start_date")) %>%
+                semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+                import_actes(x)
+
+              readr::read_csv("/manip/mensurations17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+                stats::setNames(c("patient_ide", "encounter_ide", "poids", "taille", "IMC")) %>%
+                semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+                import_mensurations(patients, x)
+
+              readr::read_csv("/manip/bios17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+                stats::setNames(c("patient_ide", "encounter_ide", "start_date", "concept_cd", "nval_num")) %>%
+                semi_join(patients, by = c("patient_ide", "encounter_ide")) %>%
+                import_bios(patients, x)
+
+        })
+}
+
+
 pop_chru <- function()
 {
-  readr::read_csv("~/2016/pims16.csv", col_types = readr::cols(.default = readr::col_character())) -> patients
+  readr::read_csv("/manip/pims16.csv", col_types = readr::cols(.default = readr::col_character())) -> patients
 
   patients %>%
   import_patients_visits("CHRU")
 
-  readr::read_csv("~/2016/diags16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+  readr::read_csv("/manip/diags16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
   import_diagnostics("CHRU")
 
-  readr::read_csv("~/2016/actes16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+  readr::read_csv("/manip/actes16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
   import_actes("CHRU")
 
-  readr::read_csv("~/2016/mensurations16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+  readr::read_csv("/manip/mensurations16.csv", col_types = readr::cols(.default = readr::col_character())) %>%
   import_mensurations(patients, "CHRU")
 
-  readr::read_csv("~/2016/bio16_1.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+  readr::read_csv("/manip/bio16_1.csv", col_types = readr::cols(.default = readr::col_character())) %>%
   import_bios(patients, "CHRU")
 
-  readr::read_csv("~/2016/bio16_2.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+  readr::read_csv("/manip/bio16_2.csv", col_types = readr::cols(.default = readr::col_character())) %>%
   import_bios(patients, "CHRU")
 
-  readr::read_csv("~/2016/pims17.csv", col_types = readr::cols(.default = readr::col_character())) -> patients
+  readr::read_csv("/manip/pims17.csv", col_types = readr::cols(.default = readr::col_character())) -> patients
 
   patients %>%
   import_patients_visits("CHRU")
 
-  readr::read_csv("~/2016/diags17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+  readr::read_csv("/manip/diags17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
   import_diagnostics("CHRU")
 
-  readr::read_csv("~/2016/actes17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+  readr::read_csv("/manip/actes17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
   import_actes("CHRU")
 
-  readr::read_csv("~/2016/mensurations17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+  readr::read_csv("/manip/mensurations17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
   import_mensurations(patients, "CHRU")
 
-  readr::read_csv("~/2016/bios17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
+  readr::read_csv("/manip/bios17.csv", col_types = readr::cols(.default = readr::col_character())) %>%
   import_bios(patients, "CHRU")
 }
 
